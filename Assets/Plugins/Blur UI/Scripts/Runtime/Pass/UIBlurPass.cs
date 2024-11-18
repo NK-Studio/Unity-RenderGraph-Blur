aef904f88b8eb3ec6e7c0ca2c0c24e28bd02e8f9
NK Studio <NK-Studio@users.noreply.github.com>
2024-11-18 21:47:31 +0900

    주석 설명 변경, AllowPassCulling 내용 수정

--- Assets/Plugins/Blur UI/Scripts/Runtime/Pass/UIBlurPass.cs
+++ Assets/Plugins/Blur UI/Scripts/Runtime/Pass/UIBlurPass.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using JetBrains.Annotations;
+using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.RenderGraphModule;
 using UnityEngine.Rendering.Universal;
@@ -59,11 +60,8 @@ namespace NKStudio
         // 이 정적 메서드는 패스를 실행하는 데 사용되며 RenderGraph 렌더 패스에 RenderFunc 대리자로 전달됩니다.
         static void ExecuteMipmapPass(MipMapPassData data, UnsafeGraphContext context)
         {
-            if (data.AlwaysShow || Application.isPlaying)
-            {
+            if (data.AlwaysShow || Application.isPlaying) 
                 context.cmd.SetGlobalTexture(OriginTexPropertyName, data.Source);
-                context.cmd.SetGlobalFloat(BlurOffsetPropertyName, data.BlurOffset);   
-            }
             
             var unsafeCmd = CommandBufferHelpers.GetNativeCommandBuffer(context.cmd);
             var source = data.Source;
@@ -79,7 +77,10 @@ namespace NKStudio
             }
 
             if (data.AlwaysShow || Application.isPlaying)
+            {
+                context.cmd.SetGlobalFloat(BlurOffsetPropertyName, data.BlurOffset);  
                 context.cmd.SetGlobalTexture(BlurTexPropertyName, source);
+            }
         }
 
         public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
@@ -88,7 +89,7 @@ namespace NKStudio
             // 활성 색상 및 깊이 텍스처는 카메라가 렌더링하는 기본 색상 및 깊이 버퍼입니다.
             UniversalResourceData resourceData = frameData.Get<UniversalResourceData>();
 
-            // add a raster render pass to the render graph, specifying the name and the data type that will be passed to the ExecutePass function
+            // ExecutePass 함수에 전달될 이름과 데이터 유형을 지정하여 렌더 그래프에 렌더 패스를 추가합니다.
             using (var builder = renderGraph.AddUnsafePass<MipMapPassData>("Blur UI Mipmap", out var passData))
             {
                 // 플레이 모드가 되지 않아도 동작시킬지 여부를 설정합니다.
@@ -97,24 +98,26 @@ namespace NKStudio
                 // 프레임 데이터를 통해 활성 색상 텍스처를 가져오고 이를 블릿의 소스 텍스처로 설정합니다.
                 passData.Source = resourceData.activeColorTexture;
 
-                // Setup Material
+                // 블러 오프셋을 설정합니다.
+                passData.BlurOffset = _blurOffset;
+                
+                // 머티리얼을 설정합니다.
                 passData.TargetMaterial = _material;
-
+                
+                // 반복 횟수의 2배로 만들어서 절반은 다운 샘플링으로 활용하고, 나머지 절반은 업 샘플링으로 활용합니다.
+                int scratchesCount = Mathf.Max(_blurIteration * 2, 1);
+                passData.Scratches = new TextureHandle[scratchesCount];
+                
                 // 텍스처의 설명을 가져와서 수정합니다.
                 var descriptor = passData.Source.GetDescriptor(renderGraph);
                 descriptor.msaaSamples = MSAASamples.None; // blit 작업에 대해 MSAA를 비활성화합니다.
                 descriptor.clearBuffer = false;
                 
-                // 반복 횟수의 2배로 만들어서 절반은 다운 샘플링으로 활용하고, 나머지 절반은 업 샘플링으로 활용합니다.
-                int scratchesCount = Mathf.Max(_blurIteration * 2, 1);
-
+                // 소스 텍스처의 너비와 높이를 가져옵니다. (예 : 모니터가 FHD일 경우 1920x1080으로 처리 됩니다.)
                 int sourceSizeWidth = descriptor.width;
                 int sourceSizeHeight = descriptor.height;
-
-                passData.Scratches = new TextureHandle[scratchesCount];
-                passData.BlurOffset = _blurOffset;
-
-                // 다운 샘플링 Blit 반복
+                
+                // 다운/업 샘플링 텍스처 생성
                 for (int i = 0; i < scratchesCount-1; i++)
                 {
                     int downsampleIndex = SimplePingPong(i, _blurIteration - 1);
@@ -126,18 +129,17 @@ namespace NKStudio
                     builder.UseTexture(passData.Scratches[i], AccessFlags.ReadWrite);
                 }
                 
-                // 최종 스케일 업 결과물
+                // 마지막 텍스처는 원본 텍스처의 크기와 동일합니다.
                 descriptor.width = sourceSizeWidth;
                 descriptor.height = sourceSizeHeight;
                 descriptor.name = $"Blur UI Mipmap_{scratchesCount-1}";
                 passData.Scratches[scratchesCount-1] = renderGraph.CreateTexture(descriptor);
                 builder.UseTexture(passData.Scratches[scratchesCount-1], AccessFlags.ReadWrite);
 
-                // UseTexture()를 통해 src 텍스처를 이 패스에 대한 입력 종속성으로 선언합니다.
+                // 화면 컬러 텍스처를 사용함을 선언
                 builder.UseTexture(resourceData.activeColorTexture);
-
-                // 일반적으로 이 패스가 컬링되므로 이 샘플의 시연 목적으로 이 패스에 대한 컬링을 비활성화합니다.
-                // 대상 텍스처는 다른 곳에서는 사용되지 않기 때문에
+                
+                // 최적화로 인해 Pass가 컬링되는 것을 방지합니다.
                 builder.AllowPassCulling(false);
 
                 // 패스를 실행할 때 렌더 그래프에 의해 호출되는 렌더 패스 대리자에 ExecutePass 함수를 할당합니다.
